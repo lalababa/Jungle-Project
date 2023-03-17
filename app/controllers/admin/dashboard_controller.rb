089828c7e766f4d89fb383ccff645158e1f4c46d
lalababa <lalababadidi@gmail.com>
2023-03-17 10:25:44 -0400

    add admin authencatation

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "sample", password: "sample"
+
   def show
     @product_count = Product.count
 
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: "sample", password: "sample"
 
   def index
     @products = Product.order(id: :desc).all
